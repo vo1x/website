e59401a38d3115c8b5bfc75e1cf6dd1285670ce2
vo1x <108168108+itstazdev@users.noreply.github.com>
2022-06-25 16:44:04 +0545

    Removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,16 @@ import React from "react";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Footer from "./components/footer/Footer";
-import Blog from "./components/blog/Blog";
-import Post1 from "./components/blog/posts/hello-world";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <>
       <div className="content-wrapper">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/hello-world" element={<Post1 />} />
-        </Routes>
+        <Home />
       </div>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 };
 
